b9cbff68728541c90e6431d8cefc0221458bb167
dyamorph <dyamorph22@gmail.com>
2023-04-21 13:10:14 +0300

    complete task-9

---
+++ task-9.php
@@ -0,0 +1,41 @@
+<?php
+class Student {
+    public string $firstName;
+    public string $lastName;
+    public string $group;
+    public float $averageMark;
+
+    public function __construct($firstName, $lastName, $group, $averageMark) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->group = $group;
+        $this->averageMark = $averageMark;
+    }
+
+    public function getScholarship():int {
+        return ($this->averageMark == 5) ? 100 : 80;
+    }
+}
+class Aspirant extends Student {
+    protected string $researchTopic;
+
+    public function __construct($firstName, $lastName, $group, $averageMark, $researchTopic) {
+        parent::__construct($firstName, $lastName, $group, $averageMark);
+        $this->researchTopic = $researchTopic;
+    }
+
+    public function getScholarship():int {
+        return ($this->averageMark == 5) ? 200 : 180;
+    }
+}
+$aspirant = new Aspirant("John", "Doe", "G1", 5.0, "ML");
+$students = [
+    new Student("Alice", "Smith", "B2", 4.5),
+    new Student("Bob", "Johnson", "C3", 5.0),
+    $aspirant,
+    new Aspirant("Frank", "K", "R1", 4.0, "Backend"),
+    new Aspirant("Billy", "Herrington", "A5", 5.0, "Frontend"),
+];
+foreach ($students as $student) {
+    echo $student->getScholarship() . " USD\n";
+}
